70e9791d895cbdc42f0a58192415833e3dc391e7
groupeddm <jeanfrancois.drapeau@groupe-ddm.com>
2016-10-26 09:57:23 -0400

    Starting loop mode coding

--- autotorrent/cmd.py
+++ autotorrent/cmd.py
@@ -235,7 +235,7 @@ def commandline_handler():
                             if seeding:
                                 # Add to cross-seed
                                 print('!  Adding torrent in cross-seed mode')
-                                addtfile(at, args.loopmode, set(fn), args.dry_run)
+                                addtfile(at, args.loopmode, list(fn), args.dry_run)
                                 # delete torrent file
                                 print('!  Deleting file')
                                 os.remove(os.path.join(args.loopmode, fn))

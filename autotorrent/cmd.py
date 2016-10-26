01a77af84657315b3961418da59222fa9b89b27f
groupeddm <jeanfrancois.drapeau@groupe-ddm.com>
2016-10-26 11:13:31 -0400

    Starting loop mode coding

--- autotorrent/cmd.py
+++ autotorrent/cmd.py
@@ -331,6 +331,7 @@ def addtfile(at, current_path, afiles, adry_run, is_new):
                 print('')
 
 if __name__ == '__main__':
+    dsdssdds
     print('###### autotorrent-1.6.2e1 ######')
     print('# Original by John Doee https://github.com/JohnDoee/autotorrent')
     print('# Monitoring mode added by Jean-Francois Drapeau https://github.com/jeanfrancoisdrapeau/autotorrent')
